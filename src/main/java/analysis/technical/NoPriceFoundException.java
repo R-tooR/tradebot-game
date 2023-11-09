3af63b1da255ea27ad44ffc102985f7c5c2b6d46
artur <arturbugaj37@gmail.com>
2023-11-09 09:33:51 +0100

    resolve sonar local analysis

--- src/main/java/analysis/technical/NoPriceFoundException.java
+++ src/main/java/analysis/technical/NoPriceFoundException.java
@@ -1,6 +1,12 @@
 package analysis.technical;
 
+import java.io.Serial;
+
 public class NoPriceFoundException extends Exception {
+
+    @Serial
+    private static final long serialVersionUID = 124123532L;
+
     public NoPriceFoundException(String message) {
         super(message);
     }
